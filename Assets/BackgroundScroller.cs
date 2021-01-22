37cc37271563161df934f03a685cdeab2a08d1e8
TheTiodan <timo-kleinschnitz@gmx.de>
2021-01-22 15:43:04 +0100

    Initial commit

---
+++ Assets/BackgroundScroller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] float backGroundScrollSpeed = 0.5f;
+    Material myMaterial;
+    Vector2 offSet;
+
+    private void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+        offSet = new Vector2(0f, backGroundScrollSpeed);
+    }
+    private void Update()
+    {
+        myMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
---
+++ Assets/Level.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    public void LoadStartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
